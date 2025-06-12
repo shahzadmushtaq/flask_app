716029ae576cc489a4675a844dd38240c4d5b08f
Shahzad Mushtaq <68580634+shahzadmushtaq@users.noreply.github.com>
2025-06-12 16:11:45 +0500

    Create data_models.py

---
+++ data_models.py
@@ -0,0 +1,45 @@
+from sqlmodel import SQLModel , Field
+from sqlmodel import create_engine , Session , select
+
+class Phones(SQLModel,table=True):
+    id : int = Field(primary_key=True)
+    title : str  = Field(default=None)
+    price : int | None = Field(default=0)
+    currency : str | None = Field(default="usd")
+    ram : int | None = Field(default=None)
+    storage : int | None = Field(default=None)
+
+
+# create engine to communicate to database
+engine = create_engine("sqlite:///database.db")
+
+#Initialize the table creations
+SQLModel.metadata.create_all(engine)
+def add_record(phone_id,title,price,currency,ram,storage):
+    data_row = Phones(
+        id=phone_id,
+        title=title,
+        price=price,
+        currency=currency,
+        ram=ram,
+        storage=storage
+    )
+    with Session(engine) as session:
+        session.add(data_row)
+        session.commit()
+    print("Record added to database.")
+
+def get_data_by_id(phone_id):
+    data_row = None
+    with Session(engine) as session:
+        statement = select(Phones).where(Phones.id == phone_id)
+        row = session.exec(statement).first()
+        data_row=row
+    return data_row
+def get_all():
+    data = None
+    with Session(engine) as session:
+        statement = select(Phones)
+        rows = session.exec(statement).fetchall()
+        data=rows
+    return data
