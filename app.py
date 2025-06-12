9220ca6495118cdc782fc86736547dffe9577a3d
Shahzad Mushtaq <68580634+shahzadmushtaq@users.noreply.github.com>
2025-06-12 16:25:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -14,15 +14,21 @@ def get():
 
 @app.route("/phones",methods=["GET"])
 def get_phones():
-    data = get_all()
-    data_parsed = [{"id":dat.id,"Title":dat.title,"Price":dat.price,"Currency":dat.currency,"Ram":dat.ram,"Storage":dat.storage} for dat in data]
-    return {"status":"success","Products": data_parsed }
+    try:
+        data = get_all()
+        data_parsed = [{"id":dat.id,"Title":dat.title,"Price":dat.price,"Currency":dat.currency,"Ram":dat.ram,"Storage":dat.storage} for dat in data]
+        return {"status":"success","Products": data_parsed }
+    except Exception:
+        return {"status":"failed", "Products": []}
 
 @app.route("/phones/<phone_id>",methods=["GET"])
 def phone_by_id(phone_id):
-    data = get_data_by_id(phone_id)
-    record = {"id":data.id,"Title":data.title,"Price":data.price,"Currency":data.currency,"Ram":data.ram,"Storage":data.storage}
-    return {"status" : "success", "Data" : record}
+    try:
+        data = get_data_by_id(phone_id)
+        record = {"id":data.id,"Title":data.title,"Price":data.price,"Currency":data.currency,"Ram":data.ram,"Storage":data.storage}
+        return {"status" : "success", "Data" : record}
+    except Exception:
+        return {"status":"failed","Data":None}
 
 
 if __name__ == "__main__":
